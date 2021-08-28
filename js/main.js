034f487987269e5184668b4e9864b3ffae2ac1f1
Andy Chiang <andy10801@gmail.com>
2021-08-28 19:35:34 +0800

    add template

--- js/main.js
+++ js/main.js
@@ -1,12 +1,12 @@
-//Get the button
 let mybutton = document.getElementById("btn-back-to-top");
 
-// When the user scrolls down 20px from the top of the document, show the button
 window.onscroll = function () {
     scrollFunction();
 };
 
 function scrollFunction() {
+    console.log(document.body.scrollTop)
+    console.log(document.documentElement.scrollTop)
     if (
         document.body.scrollTop > 500 ||
         document.documentElement.scrollTop > 500
@@ -16,7 +16,7 @@ function scrollFunction() {
         mybutton.style.display = "none";
     }
 }
-// When the user clicks on the button, scroll to the top of the document
+
 mybutton.addEventListener("click", backToTop);
 
 function backToTop() {
